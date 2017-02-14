22bf7147b17d2f45d01b81c9eed4bb634565efba
duks <duks@robooot.com>
2017-02-14 11:10:03 +0800

    Initial Commit

---
+++ DKSWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKSWebView
+//
+//  Created by aDu on 2017/2/14.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DKSWebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DKSWebView
+//
+//  Created by aDu on 2017/2/14.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
